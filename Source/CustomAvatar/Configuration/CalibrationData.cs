344a079cbfd5dae3e1a2d8da4db0f8b373a4149f
Nicolas Gnyra <nicolas@gnyra.com>
2020-08-07 23:55:59 -0400

    Actually skip missing files

--- Source/CustomAvatar/Configuration/CalibrationData.cs
+++ Source/CustomAvatar/Configuration/CalibrationData.cs
@@ -18,7 +18,7 @@ namespace CustomAvatar.Configuration
 
         private readonly Dictionary<string, FullBodyCalibration> _manualCalibration = new Dictionary<string, FullBodyCalibration>();
 
-        private ILogger<CalibrationData> _logger;
+        private readonly ILogger<CalibrationData> _logger;
 
         public CalibrationData(ILoggerProvider loggerProvider)
         {
@@ -83,6 +83,8 @@ namespace CustomAvatar.Configuration
 
                 int count = reader.ReadInt32();
 
+                _logger.Trace($"Reading {count} calibrations");
+
                 for (int i = 0; i < count; i++)
                 {
                     string fileName = reader.ReadString();
@@ -90,6 +92,7 @@ namespace CustomAvatar.Configuration
                     if (!IsValidFileName(fileName))
                     {
                         _logger.Warning($"'{fileName}' is not a valid file name; skipped");
+                        continue;
                     }
 
                     string fullPath = Path.Combine(PlayerAvatarManager.kCustomAvatarsPath, fileName);
@@ -97,7 +100,10 @@ namespace CustomAvatar.Configuration
                     if (!File.Exists(fullPath))
                     {
                         _logger.Warning($"'{fullPath}' no longer exists; skipped");
+                        continue;
                     }
+
+                    _logger.Trace($"Got calibration data for '{fileName}'");
 
                     if (!_manualCalibration.ContainsKey(fileName))
                     {
